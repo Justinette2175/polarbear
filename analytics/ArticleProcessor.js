355a6b6e0fa168b034f82036cb935a28fa2a3427
cedriclab <cedric.levlab@gmail.com>
2018-03-23 21:47:28 -0400

    Now able to load all data for given article ID

--- analytics/ArticleProcessor.js
+++ analytics/ArticleProcessor.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird');
 const RequestPromise = require('request-promise');
+const striptags = require('striptags');
 
 const processComments = require('./CommentsProcessor');
 
@@ -7,6 +8,8 @@ const ACCESS_KEY = 'bf9ac6d8-9ad8-4124-a63c-7b7bdf22a2ee';
 const RC_NEURO_URL = 'https://services.radio-canada.ca/hackathon/neuro/v1';
 const RC_SITESEARCH_URL = 'https://services.radio-canada.ca/hackathon/sitesearch/v1';
 const RC_VALIDATION_URL = 'http://api.radio-canada.ca/validationMedia/v1/Validation.html';
+
+const BASE_RC_URL = 'http://ici.radio-canada.ca';
 
 const getValidationParams = function (params) {
   return Object.assign({
@@ -27,3 +30,19 @@ const loadArticleMetadata = function (id) {
     json: true
   });
 };
+
+const processArticle = function (id) {
+  return loadArticleMetadata(id)
+    .then((metadata) => {
+      return processComments(metadata.canonicalWebLink.href.replace(BASE_RC_URL, ''))
+        .then((commentData) => {
+          return Object.assign({id}, {
+            title: metadata.title,
+            summary: striptags(metadata.summary),
+            text: striptags(metadata.body.html)
+          }, commentData);
+        })
+    });
+};
+
+module.exports = processArticle;
