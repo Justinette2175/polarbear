09af2e604b287d8ad86086bf437c0f44d7fb185d
cedriclab <cedric.levlab@gmail.com>
2018-03-25 09:26:20 -0400

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
