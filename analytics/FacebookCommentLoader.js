885a387c14761cb63230831c88733f35068df4a5
cedriclab <cedric.levlab@gmail.com>
2018-03-24 21:53:43 -0400

    data and attempt at fb

--- analytics/FacebookCommentLoader.js
+++ analytics/FacebookCommentLoader.js
@@ -1,21 +1,43 @@
 const Promise = require('bluebird');
 const RequestPromise = require('request-promise');
 
-const FACEBOOK_BASE_API = 'https://graph.facebook.com/v2.12/';
+const fs = require('fs');
+
+const FACEBOOK_BASE_API = 'https://graph.facebook.com/v2.12';
 
 const TIMEOUT = 2000;
 
 const loadFacebookLink = function (link) {
   return RequestPromise({
-    uri: `${FACEBOOK_BASE_API}/sharedposts`,
+    uri: `${FACEBOOK_BASE_API}/${encodeURIComponent(link)}`,
     qs: {
-      id: link,
-      fields: 'engagement',
-      access_token: 'EAACEdEose0cBAO4hvLm7ULpzMBQOBGSAaITkVwmHe8C72bTCQYZCZBYdmkDYEVvaFIvVNWWZCNxCWfhOuAFDChPezjVKDilHMi4dCpl7O2UUGAtpHiaS9fRiZAGblrFEdAKDfyP6Hktnd5sFMjLZCyeXeGl9FQ3ZC5OeqSZA45GblWw8rXNZBXZAl1bQG4rsP6p4ZD'
+      fields: 'og_object',
+      access_token: 'EAACEdEose0cBAJLcLQ2ptXm5rtileIUzd27iZAlQpmzRxak5uANVvq9dZAGfrKZALErjrBeQATIkL360sWvKCrPU0XtN49q3gAtRi5PZCu18QfSejZCd1GeHKlw19NOQp5L0h8fCUiAFs5ZBoJ2V5gwcRiC2ZCAFHai6yHNfk5ZCM7ONipW8XOie1JRvldyLzrO8C0GTJ3TGkgZDZD'
     },
     json: true,
     timeout: TIMEOUT
-  });
+  })
+  .then((ogObject) => {
+    return RequestPromise({
+      uri: `${FACEBOOK_BASE_API}/${ogObject.og_object.id}/comments`,
+      qs: {
+        access_token: 'EAACEdEose0cBAJLcLQ2ptXm5rtileIUzd27iZAlQpmzRxak5uANVvq9dZAGfrKZALErjrBeQATIkL360sWvKCrPU0XtN49q3gAtRi5PZCu18QfSejZCd1GeHKlw19NOQp5L0h8fCUiAFs5ZBoJ2V5gwcRiC2ZCAFHai6yHNfk5ZCM7ONipW8XOie1JRvldyLzrO8C0GTJ3TGkgZDZD'
+      },
+      json: true,
+      timeout: TIMEOUT
+    })
+    .then((commentData) => {
+      return {
+        url: link,
+        postId: ogObject.og_object.id,
+        data: commentData.data
+      };
+    });
+  })
+  .catch((e) => {
+    console.log(e.message);
+    return {data: []};
+  })
 }
 
 const loadLaPresseArticles = function () {
@@ -36,10 +58,15 @@ loadLaPresseArticles()
     return Promise.mapSeries(articles, (article, index) => loadFacebookLink(article.url).then((data) => {
       if (data && data.data && data.data.length) {
         console.log("DATA FOR", article.url);
+        return data;
       } else {
         console.log("Nothing", index, "/", count);
+        return null;
       }
     }).catch(() => null));
   })
-  .then(() => console.log("DONE"))
+  .then((data) => {
+    fs.writeFileSync(`${__dirname}/data/fb-comments.json`, JSON.stringify(data.filter((x) => !!x)), {encoding: 'utf8'});
+    console.log("wrote to file");
+  })
   .catch((e) => console.error(e.message));
