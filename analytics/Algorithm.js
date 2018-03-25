55d6845c1e1e00c86e45098e2a74b5610576056f
cedriclab <cedric.levlab@gmail.com>
2018-03-25 01:10:33 -0400

    Added small server for fake algorithm

--- analytics/Algorithm.js
+++ analytics/Algorithm.js
@@ -1,8 +1,11 @@
+const Promise = require('bluebird');
+
 const DUMMY_TONE_WEIGHTS = {
   'trump': -20,
   'terrorisme': -15,
   'politique': -10,
   'trudeau': -5,
+  'couillard': -10,
   'canada': 10,
   'chat': 20,
   'spacex': 15,
@@ -10,19 +13,30 @@ const DUMMY_TONE_WEIGHTS = {
   'chaton': 30,
   'mort': -10,
   'décès': -5,
-  'armes': -20,
-  'guerre': -30
+  'arme': -20,
+  'guerre': -30,
+  'hydro-québec': -20,
+  'média': -10,
+  'tramway': 5
 };
 const DUMMY_POLARISATION_WEIGHTS = {
   'trump': 50,
-  'terrorisme': 50,
-  'politique': -10,
+  'terrorisme': 60,
+  'politique': 50,
   'trudeau': 50,
+  'couillard': 10,
   'canada': 10,
-  'chat': 20,
-  'chaton': 30,
-  'mort': -10,
-  'décès': -5
+  'chat': -20,
+  'spacex': 30,
+  'musk': 30,
+  'chaton': -30,
+  'mort': 10,
+  'décès': -10,
+  'arme': 20,
+  'guerre': 15,
+  'hydro-québec': 10,
+  'média': 40,
+  'tramway': 40
 };
 
 const wordify = function (word) {
@@ -50,32 +64,62 @@ const createWordCounts = function (title, summary, text) {
         internalAcc[word] += value;
 
         return internalAcc;
-      });
+      }, acc);
   }, {});
+};
+
+const findWordWeight = function (word, weights) {
+  const keys = Object.keys(weights);
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i].indexOf(word) === 0) {
+      return weights[keys[i]];
+    }
+  }
+  return 0;
 };
 
 const computeScore = function(wordCount, weights) {
   return Object.keys(wordCount).reduce((acc, word) => {
-    return acc += (weights[word] || 0) * wordCount[word];
-  });
+    return acc += (findWordWeight(word, weights) * wordCount[word]);
+  }, 0);
 };
 
 const logistic = function (value) {
   return 1 / (1 + Math.exp(-1 * value));
 };
 
-const analyseText = function (text, summary, text) {
-  const words = createWordCounts(text, summary, text);
+const analyseText = function (title, summary, text) {
+  return Promise.try(() => {
+    const wordCounts = createWordCounts(title, summary, text);
+    const tone = logistic(computeScore(wordCounts, DUMMY_TONE_WEIGHTS));
+    const toneDistFromExtrema = 1 - (Math.abs(tone - 0.5) + 0.5);
 
-  return Promise.resolve({
-    engagements: {
-      reactions: 0,
-      shares: 0,
-      comments: 0
-    },
-    tone: {
-      average: logistic(computeScore(words, DUMMY_TONE_WEIGHTS)),
-      stdDev: logistic(computeScore(words, DUMMY_POLARISATION_WEIGHTS)),
-    }
+    return {
+      engagement: {
+        reactions: 0,
+        shares: 0,
+        comments: 0
+      },
+      tone: {
+        average: tone,
+        stdDev: logistic(computeScore(wordCounts, DUMMY_POLARISATION_WEIGHTS)) * toneDistFromExtrema * 2,
+      }
+    };
   })
+  .catch(() => {
+    return {
+      engagement: {
+        reactions: 0,
+        shares: 0,
+        comments: 0
+      },
+      tone: {
+        average: Math.random(),
+        stdDev: Math.random(),
+        ramdom: true
+      }
+    };
+  });
 };
+
+exports.analyseText = analyseText;
---
+++ server.js
@@ -0,0 +1,50 @@
+const Promise = require('bluebird');
+const RequestPromise = require('request-promise');
+
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Algorithm = require('./analytics/Algorithm');
+
+const loadModel = function(title, summary, text) {
+  return RequestPromise({
+    uri: 'http://localhost:8081/predict',
+    body: {
+      title,
+      summary,
+      text
+    },
+    json: true,
+    timeout: 2000
+  });
+};
+
+app.post('/analysis', (req, res) => {
+  return Algorithm.analyseText(req.body.title, req.body.summary, req.body.text)
+    .then((data) => {
+      return loadModel(req.body.title, req.body.summary, req.body.text)
+        .then((modelData) => {
+          data.engagement.comments = modelData.comments;
+          data.engagement.shares = modelData.shares;
+          data.engagement.reactions = modelData.reactions;
+
+          return data;
+        })
+        .catch(() => data);
+    })
+    .then((completeData) => {
+      res.status(200);
+      return res.json(completeData);
+    })
+    .catch((e) => {
+      console.error(e.message);
+      res.status(500);
+      res.send();
+    })
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
